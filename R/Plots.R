27af34f8bd96060e6dea57a02f1ef857bb864ca5
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-11-23 11:00:59 +0100

    Add Plot segmentation funtion

--- R/Plots.R
+++ R/Plots.R
@@ -15,6 +15,7 @@
 #' plotTree(results$tree)
 #' @export
 #' @import ggplot2
+#' @importFrom ggplot2 ggplot geom_segment geom_label scale_fill_manual theme_void
 plotTree <- function(tree){
   DF=tree
   n=NROW(DF)
@@ -51,6 +52,58 @@ plotTree <- function(tree){
     ggplot2::geom_label(aes(x,y,label=indx,fill=isTerminal),size=5,color='white')+
     ggplot2::scale_fill_manual(values=c('royalblue3','red3'))+
     ggplot2::theme_void()
+  return(g)
+}
+
+#' Plot segmentation
+#'
+#' Plot the final segmentation displaying shift time along with uncertainties
+#'
+#' @param data list, summary data resulting from any segmentation function
+#' @param shift list, summary shift resulting from any segmentation function
+#'
+#' @return a ggplot
+#'
+#' @examples
+#' # Apply recursive segmentation
+#' results=recursive.segmentation(obs=RhoneRiver$H,time=RhoneRiver$Year,u=RhoneRiver$uH)
+#'
+#' # plot recursive segmentation
+#' plotSegmentation(data=results$summary$data,
+#'                  shift=results$summary$shift)
+#' @export
+#' @importFrom  ggplot2 ggplot geom_point labs scale_color_brewer theme_bw theme geom_vline coord_cartesian geom_rect scale_fill_brewer
+plotSegmentation <- function(data,shift) {
+  # Plot observations by period
+  g=ggplot2::ggplot(data)+
+    ggplot2::geom_point(aes(x=time,
+                            y=obs,
+                            col=factor(period)))+
+    ggplot2::labs(y='Observation',
+                  col='Period',
+                  title = 'Segmentation')+
+    ggplot2::scale_color_brewer(palette = 'Dark2')+
+    ggplot2::theme_bw()+
+    ggplot2::theme(plot.title = element_text(hjust=0.5,
+                                             face='bold',
+                                             size=15),
+                   legend.title.align=0.5)
+
+  # Plot shift times
+  g=g+
+    ggplot2::geom_vline(xintercept = shift$tau,alpha=0.8)+
+    ggplot2::coord_cartesian(ylim = c(min(data$obs),max(data$obs)))+
+    ggplot2::geom_rect(data = shift,
+                       aes(xmin = I95_lower,
+                           xmax = I95_upper,
+                           ymin = -Inf,
+                           ymax = Inf,
+                           fill=(factor(tau))),
+                       alpha=0.4)+
+    ggplot2::labs(fill='Shift time')+
+    ggplot2::scale_fill_brewer(palette='Pastel1',
+                               labels=round(shift$tau,2))
+
   return(g)
 }
 
