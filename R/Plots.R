2718016a0753744129f662e09637cbf687a2fe84
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-11-24 09:53:22 +0100

    Remove importFrom and replace by import ggplot2. Add error bar to each observation

--- R/Plots.R
+++ R/Plots.R
@@ -15,7 +15,6 @@
 #' plotTree(results$tree)
 #' @export
 #' @import ggplot2
-#' @importFrom ggplot2 ggplot geom_segment geom_label scale_fill_manual theme_void
 plotTree <- function(tree){
   DF=tree
   n=NROW(DF)
@@ -46,12 +45,12 @@ plotTree <- function(tree){
   }
 
   # plot
-  g=ggplot2::ggplot(DF)+
-    ggplot2::geom_segment(aes(x=xstart,y=ystart,xend=xend,yend=yend),
+  g=ggplot(DF)+
+    geom_segment(aes(x=xstart,y=ystart,xend=xend,yend=yend),
                  arrow=arrow(type='closed',length=unit(0.03, "npc")))+
-    ggplot2::geom_label(aes(x,y,label=indx,fill=isTerminal),size=5,color='white')+
-    ggplot2::scale_fill_manual(values=c('royalblue3','red3'))+
-    ggplot2::theme_void()
+    geom_label(aes(x,y,label=indx,fill=isTerminal),size=5,color='white')+
+    scale_fill_manual(values=c('royalblue3','red3'))+
+    theme_void()
   return(g)
 }
 
@@ -59,8 +58,7 @@ plotTree <- function(tree){
 #'
 #' Plot the final segmentation displaying shift time along with uncertainties
 #'
-#' @param data list, summary data resulting from any segmentation function
-#' @param shift list, summary shift resulting from any segmentation function
+#' @param summary list, summary data resulting from any segmentation function
 #'
 #' @return a ggplot
 #'
@@ -69,40 +67,48 @@ plotTree <- function(tree){
 #' results=recursive.segmentation(obs=RhoneRiver$H,time=RhoneRiver$Year,u=RhoneRiver$uH)
 #'
 #' # plot recursive segmentation
-#' plotSegmentation(data=results$summary$data,
-#'                  shift=results$summary$shift)
+#' plotSegmentation(summary=results$summary)
 #' @export
-#' @importFrom  ggplot2 ggplot geom_point labs scale_color_brewer theme_bw theme geom_vline coord_cartesian geom_rect scale_fill_brewer
-plotSegmentation <- function(data,shift) {
+#' @import  ggplot2
+plotSegmentation <- function(summary) {
+
+  data=summary$data
+  shift=summary$shift
   # Plot observations by period
-  g=ggplot2::ggplot(data)+
-    ggplot2::geom_point(aes(x=time,
-                            y=obs,
-                            col=factor(period)))+
-    ggplot2::labs(y='Observation',
-                  col='Period',
-                  title = 'Segmentation')+
-    ggplot2::scale_color_brewer(palette = 'Dark2')+
-    ggplot2::theme_bw()+
-    ggplot2::theme(plot.title = element_text(hjust=0.5,
-                                             face='bold',
-                                             size=15),
-                   legend.title.align=0.5)
+  g=ggplot(data)+
+    geom_point(aes(x=time,
+                   y=obs,
+                   col=factor(period)))+
+    geom_errorbar(aes(x=time,
+                      y=obs,
+                      ymin=I95_lower,
+                      ymax=I95_upper,
+                      col=factor(period)),
+                  width=3)+
+    labs(y='Observation',
+         col='Period',
+         title = 'Segmentation')+
+    scale_color_brewer(palette = 'Dark2')+
+    theme_bw()+
+    theme(plot.title = element_text(hjust=0.5,
+                                    face='bold',
+                                    size=15),
+          legend.title.align=0.5)
 
   # Plot shift times
   g=g+
-    ggplot2::geom_vline(xintercept = shift$tau,alpha=0.8)+
-    ggplot2::coord_cartesian(ylim = c(min(data$obs),max(data$obs)))+
-    ggplot2::geom_rect(data = shift,
-                       aes(xmin = I95_lower,
-                           xmax = I95_upper,
-                           ymin = -Inf,
-                           ymax = Inf,
-                           fill=(factor(tau))),
-                       alpha=0.4)+
-    ggplot2::labs(fill='Shift time')+
-    ggplot2::scale_fill_brewer(palette='Pastel1',
-                               labels=round(shift$tau,2))
+    geom_vline(xintercept = shift$tau,alpha=0.8)+
+    coord_cartesian(ylim = c(min(data$obs),max(data$obs)))+
+    geom_rect(data = shift,
+              aes(xmin = I95_lower,
+                  xmax = I95_upper,
+                  ymin = -Inf,
+                  ymax = Inf,
+                  fill=(factor(tau))),
+              alpha=0.4)+
+    labs(fill='Shift time')+
+    scale_fill_brewer(palette='Pastel1',
+                      labels=round(shift$tau,2))
 
   return(g)
 }
