adc0aeb8a2e7b02aa3963a4c35e65798ee68bbf1
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-16 14:49:46 +0100

    Change some names' variables. Add updating rating curve estimation and residuals to the next iteration.

--- R/ModelAndSegment.R
+++ R/ModelAndSegment.R
@@ -18,12 +18,12 @@ recurvise.ModelAndSegmentation <- function(H,
   parents=c(0) # Vector containing the indices of the parents of each node - same size as X
   continue=TRUE # Logical determining whether recursion should continue
 
-  residualsData <- list(fitRC(time=time,H=H,Q=Q,uQ=uQ,funk=fitRC_loess))
+  residualsData <- list(fitRC(time=time,H=H,Q=Q,uQ=uQ,funk=fitRC_loess)) # initialize first residual data to be segmented
 
   if(is.null(residualsData)){
     stop('There is not enough data to run the segmentation model')
   }
-  residuals=list(residualsData[[1]]$Q_res) # List of all nodes (each corresponding to a subseries of x) to be segmented at this level. Start with a unique node corresponding to the whole series
+  residuals=list(residualsData[[1]]$Q_res) # List of all nodes (each corresponding to a subseries of residual) to be segmented at this level. Start with a unique node corresponding to the whole series
   TIME=list(residualsData[[1]]$time) # List of corresponding times
   u_residuals=list(residualsData[[1]]$uQ_sim) # List of corresponding uncertainties
 
@@ -32,7 +32,7 @@ recurvise.ModelAndSegmentation <- function(H,
     level=level+1 # Increment recursion level
     n.residuals=length(residuals) # Number of nodes at this level
     keepgoing=rep(NA,n.residuals) # Should recursion continue for each node?
-    newX=newTIME=newU=newIndices=newParents=c() # Will be used to update subseries, indices and parents at the end of each recursion level
+    new_residuals=newTIME=new_u_residuals=newIndices=newParents=c() # Will be used to update subseries, indices and parents at the end of each recursion level
     m=0 # Local counter used to control indices in the 4 vectors above => reset to 0 at each new level of the recursion
     for(j in 1:n.residuals){ # Loop on each node
       k=k+1 # Increment main counter
@@ -52,20 +52,26 @@ recurvise.ModelAndSegmentation <- function(H,
         for(i in 1:nSopt){ # Loop on each segment detected for the current node
           p=p+1 # Increment auxiliary counter
           m=m+1 # Increment local counter
-          newX[[m]]=partial.segmentation$results[[nSopt]]$data.p$obs.p[[i]] # Save ith segment (on a total of nS)
           newTIME[[m]]=partial.segmentation$results[[nSopt]]$data.p$time.p[[i]] # Save corresponding times
-          newU[[m]]=partial.segmentation$results[[nSopt]]$data.p$u.p[[i]] # Save corresponding uncertainty
           newParents[m]=indices[j] # At next level, the parent of this segment will be the index of current node
           newIndices[m]=p # At next level, the index of this segment will be p
+          NewH=residualsData[[newParents[m]]]$H[match(newTIME[[m]],residualsData[[newParents[m]]]$time)] # find information according to time segmentation
+          NewQ=residualsData[[newParents[m]]]$Q_obs[match(newTIME[[m]],residualsData[[newParents[m]]]$time)]
+          NewuQ=residualsData[[newParents[m]]]$uQ_obs[match(newTIME[[m]],residualsData[[newParents[m]]]$time)]
+          # Update rating curve estimation
+          residualsData[[p]] <- fitRC(time=newTIME[[m]],H=NewH,Q=NewQ,uQ=NewuQ,funk=fitRC_loess)
+          # update residual of new rating curve
+          new_residuals[[m]]=residualsData[[p]]$Q_res # Save ith segment (on a total of nS)
+          new_u_residuals[[m]]=residualsData[[p]]$uQ_sim # Save corresponding uncertainty
         }
       }
     }
     # Check if recursion should continue at all, i.e. if at least one node is not terminal
     if(all(keepgoing==FALSE)) continue=FALSE
     # Update list of nodes to be further segmented at next level + parents and indices
-    X=newX
+    residuals=new_residuals
     TIME=newTIME
-    U=newU
+    u_residuals=new_u_residuals
     parents=newParents
     indices=newIndices
   }
