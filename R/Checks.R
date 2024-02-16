fa5217d3c25482315c283613eac1ed82a5ed5596
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-16 16:00:33 +0100

    Description of fonction check

--- R/Checks.R
+++ R/Checks.R
@@ -1,5 +1,15 @@
-# Function to check if the vectors have the same length
-
+#' Check size vectors
+#'
+#' Function to check if the vectors have the same length
+#' @param ... real vectors
+#'
+#' @return logical, return null if the vectors have not the same length
+#' @export
+#'
+#' @examples
+#'
+#' check_vector_lengths(RhoneRiver$Time,RhoneRiver$H,RhoneRiver$Q,RhoneRiver$uQ)
+#' check_vector_lengths(RhoneRiver$Time,RhoneRiver$H,RhoneRiver$Q,RhoneRiver$uQ[-1])
 check_vector_lengths <- function(...) {
   lengths <- sapply(list(...), length)
   if (length(unique(lengths)) != 1){
