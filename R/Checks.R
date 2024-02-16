4f8f67036de0588cd959c52b8b8d617331809c00
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-16 11:08:48 +0100

    Check functions. Verify if the vectors have the same length

---
+++ R/Checks.R
@@ -0,0 +1,10 @@
+# Function to check if the vectors have the same length
+
+check_vector_lengths <- function(...) {
+  lengths <- sapply(list(...), length)
+  if (length(unique(lengths)) != 1){
+    return(NULL)
+  }else{
+    return('ok')
+  }
+}
