90aa1a0b7f4d2646c6b3fa05330d5d106af57a24
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-23 14:44:42 +0100

    Move check size function to utilities

--- R/Utilities.R
+++ R/Utilities.R
@@ -44,4 +44,23 @@ DateFormatTransform <- function(date){
   return(as.numeric(date))
 }
 
+#' Check size vectors
+#'
+#' Function to check if the vectors have the same length
+#' @param ... real vectors
+#'
+#' @return logical, return null if the vectors have not the same length
+#'
+#' @examples
+#' check_vector_lengths(RhoneRiver$Time,RhoneRiver$H,RhoneRiver$Q,RhoneRiver$uQ)
+#' check_vector_lengths(RhoneRiver$Time,RhoneRiver$H,RhoneRiver$Q,RhoneRiver$uQ[-1])
+check_vector_lengths <- function(...) {
+  lengths <- sapply(list(...), length)
+  if (length(unique(lengths)) != 1){
+    return(NULL)
+  }else{
+    return('ok')
+  }
+}
+
 
