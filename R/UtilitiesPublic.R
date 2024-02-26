b6438181b37e4677038fe6ec11f722aa50f13a47
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-26 11:09:04 +0100

    -change parse_date_time to as.POSIXct function to implement less libraries into the code (lubridate) - Separate check size function (utilities private)

--- R/UtilitiesPublic.R
+++ R/UtilitiesPublic.R
@@ -20,22 +20,37 @@
 #' datetime2
 #' as.POSIXct(datetime2, origin = '1970-01-01',tz='UTC')
 DateFormatTransform <- function(date){
-
+  # Transform date to numeric format: origin default is "1970-01-01 00:00:00 UTC"
   if(lubridate::is.Date(date)){
-    # Transform date to numeric format: origin default is "1970-01-01 00:00:00 UTC"
-    return(as.numeric(date))
+
+    # Attempt to parse the date using various formats
+    parsed_date <- as.POSIXct(date, tz = "UTC")
+
+    if(any(is.na(parsed_date)))stop('The format is not supported; please verify the input date or time format')
+
+    return(as.numeric(parsed_date))
 
   }else if(!lubridate::is.POSIXct(date)){
 
     date_string <- as.character(date)
 
     # Attempt to parse the date using various formats
-    parsed_date <- lubridate::parse_date_time(date_string,
-                                              orders = c('ymd H:M:S', 'ymd', 'mdy',
-                                                         'dmy', 'ymd HMS', 'y-m-d H:M:S',
-                                                         'y/m/d H:M:S', 'y/m/d HMS' ))
+    parsed_date <- as.POSIXct(date_string, tz = "UTC", format = c('%Y-%m-%d %H:%M:%S',
+                                             '%Y/%m/%d %H:%M:%S',
+                                             '%Y%m%d %H:%M:%S',
+                                             '%m/%d/%Y %H:%M:%S',
+                                             '%Y%m%d%H%M%S',
+                                             '%Y%m%dT%H%M%S',
+                                             '%Y-%m-%d %H:%M',
+                                             '%Y/%m/%d %H:%M',
+                                             '%Y-%m-%d',
+                                             '%Y/%m/%d',
+                                             '%Y%m%d',
+                                             '%m/%d/%Y'))
 
-    if(is.na(parsed_date))stop('The format is not supported; please verify the input date or time format')
+    parsed_date <- parsed_date[!is.na(parsed_date)][1]  # Select the first non-NA result
+
+    if(any(is.na(parsed_date)))stop('The format is not supported; please verify the input date or time format')
 
     # Transform date to numeric format: origin default is "1970-01-01 00:00:00 UTC"
     return(as.numeric(parsed_date))
@@ -48,25 +63,6 @@ DateFormatTransform <- function(date){
     date <- as.POSIXct(date, origin = '1970-01-01',tz='UTC')
   }
   return(as.numeric(date))
-}
-
-#' Check size vectors
-#'
-#' Function to check if the vectors have the same length
-#' @param ... real vectors
-#'
-#' @return logical, return null if the vectors have not the same length
-#'
-#' @examples
-#' check_vector_lengths(RhoneRiver$Time,RhoneRiver$H,RhoneRiver$Q,RhoneRiver$uQ)
-#' check_vector_lengths(RhoneRiver$Time,RhoneRiver$H,RhoneRiver$Q,RhoneRiver$uQ[-1])
-check_vector_lengths <- function(...) {
-  lengths <- sapply(list(...), length)
-  if (length(unique(lengths)) != 1){
-    return(NULL)
-  }else{
-    return('ok')
-  }
 }
 
 
