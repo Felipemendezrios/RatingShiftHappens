cb031c1ce0d61009a19ea91e0fb2c1acfa6628e5
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-28 13:59:23 +0100

    - Add exponential regression

--- R/fit.R
+++ R/fit.R
@@ -1,4 +1,4 @@
-#' fit rating curve LOESS model
+#' Fit rating curve LOESS model
 #'
 #' LOESS model used to estimate a simple rating curve
 #'
@@ -7,15 +7,18 @@
 #' @param Q real vector, discharge
 #' @param uQ real vector, uncertainty in discharge (as a standard deviation)
 #'
-#' @return data frame with the following components :
+#' @return list with the following components :
 #' \enumerate{
-#'   \item time: real value, time
-#'   \item H: real value, stage
-#'   \item Q_obs: real value, discharge observed
-#'   \item Q_sim: real value, discharge simulated
-#'   \item Q_res: real value, residual between discharge observed and simulated
-#'   \item uQ_obs: real value, uncertainty in discharge observed (as a standard deviation)
-#'   \item uQ_sim: real value, uncertainty in discharge simulated (as a standard deviation)
+#'   \item ResultsResiduals : data frame, results after fitting curve
+#'   \itemize{
+#'        \item time: real value, time
+#'        \item H: real value, stage
+#'        \item Q_obs: real value, discharge observed
+#'        \item Q_sim: real value, discharge simulated
+#'        \item Q_res: real value, residual between discharge observed and simulated
+#'        \item uQ_obs: real value, uncertainty in discharge observed (as a standard deviation)
+#'        \item uQ_sim: real value, uncertainty in discharge simulated (as a standard deviation)
+#'        }
 #' }
 #' @export
 #'
@@ -24,7 +27,8 @@
 #' subset = RhoneRiver[1:20,]
 #'
 #' # LOESS regression to estimate a simple rating curve
-#' fit=fitRC_loess(time=subset$Time,H=subset$H,Q=subset$Q,uQ=subset$uQ)
+#' fit.funk=fitRC_loess(time=subset$Time,H=subset$H,Q=subset$Q,uQ=subset$uQ)
+#' fit=fit.funk$ResultsResiduals
 #'
 #' # plot rating curve with model results
 #' plot(fit$H, fit$Q_obs, ylim = range(c(fit$Q_obs - fit$uQ_obs, fit$Q_obs + fit$uQ_obs)),
@@ -72,11 +76,11 @@ fitRC_loess<-function(time,H,Q,uQ){
                               uQ_obs=uQ,
                               uQ_sim=residual_sd
   )
-  return(ResultsResiduals)
+  return(list(ResultsResiduals=ResultsResiduals))
 }
 
 
-#' BaRatin model
+#' Fit rating curve using BaRatin model
 #'
 #' Rating curve estimated by using BaRatin approach
 #'
@@ -88,29 +92,37 @@ fitRC_loess<-function(time,H,Q,uQ){
 #' @param priors real list, prior information of each parameter
 #' @param remnant real vector, remnant model information
 #'
-#' @return real data frame
+#' @return Not yet
 fitRC_BaRatin=function(time,H,Q,uQ,matrix,priors,remnant){
   stop("fitRC_BaRatin has not yet been implemented")
 }
 
-#' fit rating curve using a linear interpolation
+#' Fit rating curve using a linear interpolation
 #'
-#' Linear interpolation used to estimate a simple rating curve
+#' Linear interpolation used to estimate a simple rating curve. Formula : \deqn{Q(h)= a \cdot H + b}
 #'
 #' @param time real vector, time
 #' @param H real vector, stage
 #' @param Q real vector, discharge
 #' @param uQ real vector, uncertainty in discharge (as a standard deviation)
 #'
-#' @return data frame with the following components :
+#' @return List with the following components :
 #' \enumerate{
-#'   \item time: real value, time
-#'   \item H: real value, stage
-#'   \item Q_obs: real value, discharge observed
-#'   \item Q_sim: real value, discharge simulated
-#'   \item Q_res: real value, residual between discharge observed and simulated
-#'   \item uQ_obs: real value, uncertainty in discharge observed (as a standard deviation)
-#'   \item uQ_sim: real value, uncertainty in discharge simulated (as a standard deviation)
+#'   \item ResultsResiduals : data frame, results after fitting curve
+#'   \itemize{
+#'        \item time: real value, time
+#'        \item H: real value, stage
+#'        \item Q_obs: real value, discharge observed
+#'        \item Q_sim: real value, discharge simulated
+#'        \item Q_res: real value, residual between discharge observed and simulated
+#'        \item uQ_obs: real value, uncertainty in discharge observed (as a standard deviation)
+#'        \item uQ_sim: real value, uncertainty in discharge simulated (as a standard deviation)
+#'        }
+#'   \item parameters : data frame, parameters of the linear interpolation expressed \deqn{Q(h)= a \cdot H + b}
+#'   \itemize{
+#'        \item a : real value, parameter of the linear interpolation
+#'        \item b : real value, parameter of the linear interpolation
+#'        }
 #' }
 #' @export
 #'
@@ -118,8 +130,12 @@ fitRC_BaRatin=function(time,H,Q,uQ,matrix,priors,remnant){
 #' # Dataset
 #' subset = RhoneRiver[1:20,]
 #'
-#' # LOESS regression to estimate a simple rating curve
-#' fit=fitRC_LinearInterpolation(time=subset$Time,H=subset$H,Q=subset$Q,uQ=subset$uQ)
+#' # Linear interpolation to estimate a simple rating curve
+#' fit.funk=fitRC_LinearInterpolation(time=subset$Time,H=subset$H,Q=subset$Q,uQ=subset$uQ)
+#' fit=fit.funk$ResultsResiduals
+#'
+#' # Parameters of the linear regression
+#' fit.funk$parameters
 #'
 #' # plot rating curve with model results
 #' plot(fit$H, fit$Q_obs, ylim = range(c(fit$Q_obs - fit$uQ_obs, fit$Q_obs + fit$uQ_obs)),
@@ -129,7 +145,7 @@ fitRC_BaRatin=function(time,H,Q,uQ,matrix,priors,remnant){
 #' arrows(fit$H, fit$Q_obs - fit$uQ_obs, fit$H, fit$Q_obs + fit$uQ_obs, angle = 90,
 #'        code = 3, length = 0.1)
 #'
-#' # Loess model regression
+#' # Linear interpolation
 #' lines(x=fit$H,y=fit$Q_sim, col="blue",lty=3, lwd=2)
 #'
 #' # plot residuals
@@ -139,15 +155,18 @@ fitRC_BaRatin=function(time,H,Q,uQ,matrix,priors,remnant){
 #'        code = 3, length = 0.1)
 #' abline(h=0, col='red')
 fitRC_LinearInterpolation <- function(time,H,Q,uQ){
-  if(length(time)<2){ # because second degree polynomial by default (loess function)
-    warning('NULL was returned because it not possible to perform LOESS regression with only two points.
-    A second degree polynomial requires at least three points for prediction')
+  if(length(time)<=2){ # because second degree polynomial by default (loess function)
+    warning('NULL was returned because it not possible to perform linear interpolation with fewer than two points.')
     return(NA)
   }
   data=data.frame(time=time,H=H,Q=Q)
 
   # Linear interpolation for estimating rating curve
   mod <- stats::lm(Q ~ H, data = data)
+
+  # Parameters
+  b= stats::coef(mod)[1]
+  a= stats::coef(mod)[2]
 
   # Prediction of discharge Q_rc from the water level (H)
   qsim <- stats::predict(mod, newdata = data.frame(data$H))
@@ -166,8 +185,125 @@ fitRC_LinearInterpolation <- function(time,H,Q,uQ){
                               Q_res=residuals,
                               uQ_obs=uQ,
                               uQ_sim=residual_sd
-  )
-  return(ResultsResiduals)
+                              )
+
+  parameters=data.frame(a=a,b=b)
+
+  return(list(ResultsResiduals=ResultsResiduals,
+              parameters=parameters))
+}
+
+#' Fit rating curve using an exponential regression
+#'
+#' Exponential regression expressed as \deqn{Q(h)=a \cdot \exp(b \cdot H)}
+#'
+#' @param time real vector, time
+#' @param H real vector, stage
+#' @param Q real vector, discharge
+#' @param uQ real vector, uncertainty in discharge (as a standard deviation)
+#'
+#' @return List with the following components :
+#' \enumerate{
+#'   \item ResultsResiduals : data frame, results after fitting curve
+#'   \itemize{
+#'        \item time: real value, time
+#'        \item H: real value, stage
+#'        \item Q_obs: real value, discharge observed
+#'        \item Q_sim: real value, discharge simulated
+#'        \item Q_res: real value, residual between discharge observed and simulated
+#'        \item uQ_obs: real value, uncertainty in discharge observed (as a standard deviation)
+#'        \item uQ_sim: real value, uncertainty in discharge simulated (as a standard deviation)
+#'        }
+#'   \item parameters : data frame, parameters of the exponential regression expressed \deqn{Q(h)=Q0 \cdot \exp(mu \cdot H)}
+#'   \itemize{
+#'        \item Q0 : real value, parameter of exponential model regression
+#'        \item mu : real value, parameter of exponential model regression
+#'        }
+#' }
+#' @export
+#'
+#' @examples
+#' # Dataset
+#' subset = RhoneRiver[1:20,]
+#'
+#' # Exponential regression to estimate a simple rating curve
+#' fit.all=fitRC_exponential(time=subset$Time,H=subset$H,Q=subset$Q,uQ=subset$uQ)
+#'
+#' fit=fit.all$ResultsResiduals
+#' fit.param=fit.all$parameters
+#'
+#' # Parameters of the exponential regression
+#' fit.param
+#'
+#' # plot rating curve with model results
+#' plot(fit$H, fit$Q_obs, ylim = range(c(fit$Q_obs - fit$uQ_obs, fit$Q_obs + fit$uQ_obs)),
+#'      xlab = "H", ylab = "Q_obs")
+#'
+#' # Add error bars following uQ_obs
+#' arrows(fit$H, fit$Q_obs - fit$uQ_obs, fit$H, fit$Q_obs + fit$uQ_obs, angle = 90,
+#'        code = 3, length = 0.1)
+#'
+#' # Exponential model regression
+#' curve(expr=fit.param$Q0*exp(fit.param$mu*x),from=min(fit$H),to=max(fit$H),col='blue',lwd=2,add=TRUE)
+#'
+#' # plot residuals
+#' plot(x=fit$time,y=fit$Q_res, ylim = range(c(fit$Q_res - fit$uQ_obs, fit$Q_res + fit$uQ_obs)),
+#'      xlab='H', ylab='Residual')
+#' arrows(fit$time, fit$Q_res - fit$uQ_obs, fit$time, fit$Q_res + fit$uQ_obs, angle = 90,
+#'        code = 3, length = 0.1)
+#' abline(h=0, col='red')
+fitRC_exponential <- function(time,H,Q,uQ){
+  if(length(time)<=2){
+    warning('NULL was returned because it not possible to perform exponentiel regression with only two points.
+    A second degree polynomial requires at least three points for prediction')
+    return(NA)
+  }
+  data=data.frame(time=time,H=H,Q=Q)
+
+  # Exponential model Q(h)=Q0*exp(mu*h)
+  # Convert exponential regression model to linear using the natural log of Q as the response variable and H as the predictor variable
+  # Linear model : ln(Q(h))=a1+a2*h
+  # a1 = ln(Q0) ;  a2 = mu
+  mod <- stats::lm(log(Q) ~ H, data = data)
+
+  a1=stats::coef(mod)[1]
+  a2=stats::coef(mod)[2]
+
+  # Initialization of parameters
+  Q0.init=exp(a1)
+  mu.init=a2
+
+  # Return to the exponential model
+  initialization = list(a=Q0.init, b=mu.init)
+
+  # General formula : Q(h)=a*exp(b*h)
+  model.exp=stats::nls(Q ~ a*exp(b*H), data=data, start = initialization)
+
+  Q0=stats::coef(model.exp)[1]
+  mu=stats::coef(model.exp)[2]
+
+  # Prediction of discharge Q_rc from the water level (H)
+  qsim <- stats::predict(model.exp, newdata = data.frame(data$H))
+
+  # Residual calculation
+  residuals <- data$Q - qsim
+
+  # Residual standard deviation
+  residual_sd <- stats::sd(residuals)  # incorrect interpreted as predictive uncertainty
+
+  # residual data frame
+  ResultsResiduals=data.frame(time=data$time,
+                              H=data$H,
+                              Q_obs=data$Q,
+                              Q_sim=qsim,
+                              Q_res=residuals,
+                              uQ_obs=uQ,
+                              uQ_sim=residual_sd
+                              )
+  parameters=data.frame(Q0=Q0,mu=mu)
+
+  return(list(ResultsResiduals=ResultsResiduals,
+              parameters=parameters))
 }
 
 
