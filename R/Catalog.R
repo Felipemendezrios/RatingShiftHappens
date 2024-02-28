6b11f484e143603422f4f71cc95f89bff0fc21e9
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-02-28 16:18:00 +0100

    Update getcatalog

--- R/Catalog.R
+++ R/Catalog.R
@@ -5,7 +5,7 @@
 #' @param printOnly Logical, should the catalog be returned or only printed?
 #' @return If \code{printOnly==FALSE}, a list with the following fields:
 #' \describe{
-#'   \item{models}{available models}
+#'   \item models: available models
 #' }
 #' @examples
 #' catalog <- GetCatalog()
@@ -14,7 +14,9 @@
 GetCatalog<-function(printOnly=FALSE){
   # available models
   models=c('fitRC_loess',
-           'fitRC_BaRatin')
+           'fitRC_BaRatin',
+           'fitRC_exponential',
+           'fitRC_LinearInterpolation')
   if(printOnly){
     message('MODELS:')
     print(models)
