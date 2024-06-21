2273505dc72c80effe8d18b26b294b62cb6fed8d
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-06-21 12:06:44 +0200

    Add fit function using BaRatin method with prior information on the parameters a, b and c. Limited only to a hydraulic control.

--- R/fit.R
+++ R/fit.R
@@ -293,9 +293,9 @@ fitRC_exponential <- function(time,H,Q,uQ){
               parameters=parameters))
 }
 
-#' Fit rating curve using simplified BaRatin method
+#' Fit rating curve using simplified BaRatin method with non-informative prior information with only a rectangular hydraulic control
 #'
-#' Rating curve estimated by the equation \deqn{Q(h)=a \cdot (h-b)^c} without prior information on the parameters. Only one hydraulic control is considered
+#' Rating curve estimated by the equation \deqn{Q(h)=a \cdot (h-b)^c} non-informative prior information on the parameters estimated automatically, using stage record and gauging information. Only a rectangular hydraulic control is considered
 #'
 #' @param time real vector, time
 #' @param H real vector, stage
@@ -423,28 +423,6 @@ fitRC_SimplifiedBaRatin<- function(time,H,Q,uQ,
                           doParametric=TRUE, # propagate parametric uncertainty, i.e. MCMC samples?
                           doStructural=TRUE) # propagate structural uncertainty ?
 
-  # # Define a 'prediction' object for parametric uncertainty only - not the doStructural=FALSE
-  # paramU=RBaM::prediction(X=data['H'],
-  #                         spagFiles='QRC_ParamU.spag',
-  #                         data.dir=temp.folder.RC,
-  #                         doParametric=TRUE,
-  #                         doStructural=FALSE)
-  #
-  # # Define a 'prediction' object with no uncertainty - this corresponds to the 'maxpost' RC maximizing the posterior density
-  # maxpost=RBaM::prediction(X=data['H'],
-  #                          spagFiles='QRC_Maxpost.spag',
-  #                          data.dir=temp.folder.RC,
-  #                          doParametric=FALSE,
-  #                          doStructural=FALSE)
-
-  # # Define a 'prediction' object with no uncertainty - this corresponds to the rating curve computed with prior information
-  # priorU=RBaM::prediction(X=Hgrid,
-  #                         spagFiles='QRC_Prior.spag',
-  #                         data.dir=temp.folder.RC,
-  #                         priorNsim = nrow(mcmc.segm),
-  #                         doParametric=TRUE,
-  #                         doStructural=FALSE)
-
   RBaM:: BaM(mod=M,
              data=D,
              workspace = temp.folder.RC,
@@ -477,9 +455,206 @@ fitRC_SimplifiedBaRatin<- function(time,H,Q,uQ,
   )
 
 
-  parameters=data.frame(a=summary.MCMC.MAP$a1,
-                        b=summary.MCMC.MAP$b1,
-                        c=summary.MCMC.MAP$c1,
+  parameters=data.frame(a=summary.MCMC.MAP[,which(a1$name==colnames(summary.MCMC.MAP))],
+                        b=summary.MCMC.MAP[,which(b1$name==colnames(summary.MCMC.MAP))],
+                        c=summary.MCMC.MAP[,which(c1$name==colnames(summary.MCMC.MAP))],
+                        k=summary.MCMC.MAP$k1,
+                        gamma1=summary.MCMC.MAP$Y1_gamma1,
+                        gamma2=summary.MCMC.MAP$Y1_gamma2)
+
+  # Save results from first prediction
+  copy_files_to_folder(dir.source=temp.folder.RC,
+                       dir.destination=file.path(temp.folder.RC, 'Residual'))
+
+  # Second prediction : Hgrid :
+  invisible(remove_files(dir.source = temp.folder.RC ,
+                         files_to_keep=c('Results_Cooking.txt',
+                                         'Results_Residuals.txt',
+                                         'Results_Summary.txt',
+                                         'CalibrationData.txt',
+                                         'ModelObject.RData',
+                                         'DataObject.RData',
+                                         'Residual')))
+
+  return(list(ResultsResiduals=ResultsResiduals,
+              parameters=parameters))
+
+}
+
+#' Fit rating curve using simplified BaRatin method with prior information with only a hydraulic control
+#'
+#' Rating curve estimated by the equation \deqn{Q(h)=a \cdot (h-b)^c} with prior information on the parameters. Only one hydraulic control is considered
+#'
+#' @param time real vector, time
+#' @param H real vector, stage
+#' @param Q real vector, discharge
+#' @param uQ real vector, uncertainty in discharge (as a standard deviation)
+#' @param temp.folder.RC directory, temporary directory to write computations of rating curve using observed stages and grid for plotting rating curve
+#'
+#' @return List with the following components :
+#' \enumerate{
+#'   \item ResultsResiduals : data frame, results after fitting curve
+#'   \itemize{
+#'        \item time: real value, time
+#'        \item H: real value, stage
+#'        \item Q_obs: real value, discharge observed
+#'        \item Q_sim: real value, discharge simulated
+#'        \item Q_res: real value, residual between discharge observed and simulated
+#'        \item uQ_obs: real value, uncertainty in discharge observed (as a standard deviation)
+#'        \item uQ_sim: real value, uncertainty in discharge simulated (as a standard deviation)
+#'        }
+#'   \item parameters : data frame, parameters of the simplified BaRatin model : \deqn{Q(h)=a \cdot (H - b)^ c}
+#'   \itemize{
+#'        \item a : real value, parameter of geometry component
+#'        \item b : real value, parameter of offset (thalweg or streambed)
+#'        \item c : real value, parameter of exponent
+#'        }
+#' }
+#' @export
+fitRC_SimplifiedBaRatinWithPrior<- function(time,H,Q,uQ,
+                                            a.object,
+                                            b.object,
+                                            c.object,
+                                            temp.folder.RC=file.path(tempdir(),'BaM','RC')){
+  # if(length(time)<2){
+  #   warning('NA was returned because it not possible to perform linear regression with fewer than two points.')
+  #   return(list(NA,NA))
+  # }
+
+  data=data.frame(time=time,H=H,Q=Q,uQ=uQ)
+
+  # Define the calibration dataset by specifying
+  D=RBaM::dataset(X=data['H'],
+                  Y=data['Q'],
+                  Yu=data['uQ'],
+                  data.dir=temp.folder.RC)
+
+  # Extra information to lunch model
+  controlMatrix = rbind(c(1))
+  hmax_grid = max(data$H)
+
+  # Declare a prior information about each parameter
+  b1=b.object
+
+  #initial guess a1
+  a1=a.object
+
+  c1=c.object
+
+  priors=list(b1,a1,c1)
+
+  # Config_xtra
+  xtra=RBaM::xtraModelInfo(object=list(controlMatrix,
+                                       hmax_grid)
+  )
+
+  # Stitch it all together into a model object
+  M=RBaM::model(ID='BaRatinBAC',
+                nX=1,nY=1, # number of input/output variables
+                par=priors, # list of model parameters
+                xtra=xtra) # use xtraModelInfo() to pass the control matrix
+
+  # Cooking
+  nCycles=100
+  mcmc_temp=RBaM::mcmcOptions(nCycles=nCycles)
+
+  cook_temp=RBaM::mcmcCooking(burn=0.5,
+                              nSlim=10)
+  # Error model
+  remnant_prior <- list(RBaM::remnantErrorModel(funk = "Linear",
+                                                par = list(RBaM::parameter(name="gamma1",
+                                                                           init=1,
+                                                                           prior.dist = "FlatPrior+"),
+                                                           RBaM::parameter(name="gamma2",
+                                                                           init=0.1,
+                                                                           prior.dist = "FlatPrior+"))))
+  # Run BaM executable
+  RBaM:: BaM(mod=M,
+             data=D,
+             workspace = temp.folder.RC,
+             mcmc=mcmc_temp,
+             cook = cook_temp,
+             dir.exe = file.path(find.package("RBaM"), "bin"),
+             remnant = remnant_prior)
+
+  # Save data object and model object
+  save(D,file = file.path(temp.folder.RC,'DataObject.RData'))
+  save(M,file = file.path(temp.folder.RC,'ModelObject.RData'))
+
+  # PREDICTIONS : two steps
+  # First prediction : estimate total uncertainty of simulation (u_sim = u_total) at observed stages to returned as u_sim for segmentation
+  # Second prediction : estimate total uncertainty of simulation discretized at Hgrid to plot (manage in PlotRCPrediction function)
+
+  # First prediction : observed data :
+  # Define a 'prediction' object for total predictive uncertainty only for observed stages
+  # mcmc.segm    <- utils::read.table(file=file.path(temp.folder.RC,"Results_Cooking.txt"),header=TRUE)
+  # mcmc.DIC     <- utils::read.table(file=file.path(temp.folder.RC,"Results_DIC.txt"),header=FALSE)
+  resid.segm   <- utils::read.table(file=file.path(temp.folder.RC,"Results_Residuals.txt"),header=TRUE)
+  summary.MCMC   <- utils::read.table(file=file.path(temp.folder.RC,"Results_Summary.txt"),header=TRUE)
+  summary.MCMC.MAP <- summary.MCMC[nrow(summary.MCMC),]
+
+  totalU=RBaM::prediction(X=data['H'], # stage values
+                          spagFiles='QRC_TotalU.spag', # file where predictions are saved
+                          data.dir=temp.folder.RC, # a copy of data files will be saved here
+                          doParametric=TRUE, # propagate parametric uncertainty, i.e. MCMC samples?
+                          doStructural=TRUE) # propagate structural uncertainty ?
+
+  # # Define a 'prediction' object for parametric uncertainty only - not the doStructural=FALSE
+  # paramU=RBaM::prediction(X=data['H'],
+  #                         spagFiles='QRC_ParamU.spag',
+  #                         data.dir=temp.folder.RC,
+  #                         doParametric=TRUE,
+  #                         doStructural=FALSE)
+  #
+  # # Define a 'prediction' object with no uncertainty - this corresponds to the 'maxpost' RC maximizing the posterior density
+  # maxpost=RBaM::prediction(X=data['H'],
+  #                          spagFiles='QRC_Maxpost.spag',
+  #                          data.dir=temp.folder.RC,
+  #                          doParametric=FALSE,
+  #                          doStructural=FALSE)
+
+  # # Define a 'prediction' object with no uncertainty - this corresponds to the rating curve computed with prior information
+  # priorU=RBaM::prediction(X=Hgrid,
+  #                         spagFiles='QRC_Prior.spag',
+  #                         data.dir=temp.folder.RC,
+  #                         priorNsim = nrow(mcmc.segm),
+  #                         doParametric=TRUE,
+  #                         doStructural=FALSE)
+
+  RBaM:: BaM(mod=M,
+             data=D,
+             workspace = temp.folder.RC,
+             dir.exe = file.path(find.package("RBaM"), "bin"),
+             pred=totalU, # list of predictions
+             # pred=list(totalU,paramU,maxpost), # list of predictions
+             doCalib=FALSE,
+             doPred=TRUE)
+
+  # Total uncertainty propagation
+  env_QRC_TotalU=utils::read.table(file.path(temp.folder.RC,'QRC_TotalU.env'),header=TRUE)
+
+  # Discharge simulation
+  qsim <- resid.segm$Y1_sim
+
+  # Residual calculation
+  residuals <- resid.segm$Y1_res
+
+  # Residual standard deviation
+  residual_sd <- env_QRC_TotalU$Stdev  # incorrect interpreted as predictive uncertainty
+
+  # residual data frame
+  ResultsResiduals=data.frame(time=data$time,
+                              H=data$H,
+                              Q_obs=data$Q,
+                              Q_sim=qsim,
+                              Q_res=residuals,
+                              uQ_obs=uQ,
+                              uQ_sim=residual_sd
+  )
+
+  parameters=data.frame(a=summary.MCMC.MAP[,which(a.object$name==colnames(summary.MCMC.MAP))],
+                        b=summary.MCMC.MAP[,which(b.object$name==colnames(summary.MCMC.MAP))],
+                        c=summary.MCMC.MAP[,which(c.object$name==colnames(summary.MCMC.MAP))],
                         k=summary.MCMC.MAP$k1,
                         gamma1=summary.MCMC.MAP$Y1_gamma1,
                         gamma2=summary.MCMC.MAP$Y1_gamma2)
