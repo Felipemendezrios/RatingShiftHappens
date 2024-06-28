f456defc1b69f04c63e3d09c757fc408f1a25081
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-06-28 15:39:38 +0200

    Ardeche at Meyras stage record in meters

--- data-raw/ArdecheRiverMeyrasStage.R
+++ data-raw/ArdecheRiverMeyrasStage.R
@@ -3,6 +3,6 @@ colnames(ArdecheRiverMeyrasStageRaw) <- c('NumericDate','H','Date','DateFromZero
 
 DateString<- paste(ArdecheRiverMeyrasStageRaw$Date,rep(0,length(ArdecheRiverMeyrasStageRaw$Date)),sep = ':')
 ArdecheRiverMeyrasStage <- data.frame(Date = as.POSIXct(DateString, format = '%d/%m/%Y %H:%M:%S', tz='UTC'),
-                                      H = ArdecheRiverMeyrasStageRaw$H
+                                      H = ArdecheRiverMeyrasStageRaw$H/100 # Stage record in meter
                                       )
 save(ArdecheRiverMeyrasStage,file='data/ArdecheRiverMeyrasStage.RData')
