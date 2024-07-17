dcde52fd03ec3bb32902f94710bb6a20c7d09edf
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-07-17 09:09:01 +0200

    Rename file namesof synthetic gaugings

--- data-raw/synthetic_gaugings_datasets.R
+++ data-raw/synthetic_gaugings_datasets.R
@@ -19,17 +19,17 @@ class_names_f=paste0('Shift_time_',class_names)
 
 # Separate synthetic data from shift time information and store in a list
 for(i in 1:length(all_sheets)){
-  systhetic_gauging <- c()
+  synthetic_gauging <- c()
   j=1
   # get sysnthetic data
-  systhetic_gauging[[j]] <- all_sheets[[i]][, !names(all_sheets[[i]]) %in% "shift_time"]
+  synthetic_gauging[[j]] <- all_sheets[[i]][, !names(all_sheets[[i]]) %in% "shift_time"]
   j=j+1
   # get shift time
   df_shift_time <- all_sheets[[i]][, "shift_time", drop = FALSE]
-  systhetic_gauging[[j]] <- df_shift_time[!is.na(df_shift_time$shift_time),,drop=FALSE]
+  synthetic_gauging[[j]] <- df_shift_time[!is.na(df_shift_time$shift_time),,drop=FALSE]
 
-  names(systhetic_gauging) <- c(sheet_names[i],class_names_f[i])
+  names(synthetic_gauging) <- c(sheet_names[i],class_names_f[i])
 
-  save(systhetic_gauging,
-       file=paste0('data/systhetic_gauging_',class_names[i],'.RData'))
+  save(synthetic_gauging,
+       file=paste0('data/synthetic_gauging_',class_names[i],'.RData'))
 }
