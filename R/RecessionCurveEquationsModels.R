40d261cee4f2a5d25467939878e9cbfdefb26ba0
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-10-30 11:02:27 +0100

    Equation of recession models

--- R/RecessionCurveEquationsModels.R
+++ R/RecessionCurveEquationsModels.R
@@ -1,40 +1,35 @@
-#' Recession model with two exponential and asymptotic
+#' Model 1:
 #'
-#' @param t real value, time
-#' @param alpha_1_k real value, initial stage of the first exponential function
-#' @param lambda_1 real value, fast runoff
-#' @param alpha_2_k real value, initial stage of the second exponential function
-#' @param lambda_2 real value, slow emptying of the aquifer
-#' @param beta_k real value, asymptotic stage
+#' Recession model with two exponential and asymptotic: \deqn{\alpha_{1_k} \cdot \exp(-\lambda_1 \cdot t) + \alpha_{2_k} \cdot \exp(-\lambda_2 \cdot t) + \beta_k}
 #'
+#' @return Parameter's description:
+#'
+#' \enumerate{
+#'    \item{t real value, time}
+#'    \item{alpha_1_k real value, initial stage of the first exponential function}
+#'    \item{lambda_1 real value, fast runoff}
+#'    \item{alpha_2_k real value, initial stage of the second exponential function}
+#'    \item{lambda_2 real value, slow emptying of the aquifer}
+#'    \item{beta_k real value, asymptotic stage}
+#' }
 #' @export
-Recession_M3_Equation <- function(t,alpha_1_k,lambda_1,alpha_2_k,lambda_2,beta_k){
-  alpha_1_k * exp(-lambda_1*t) + alpha_2_k * exp(-lambda_2*t) + beta_k
+Recession_M3_Equation <- function(){
+  return('alpha_1_k * exp(-lambda_1*t) + alpha_2_k * exp(-lambda_2*t) + beta_k')
 }
 
-#' Recession with one exponential and five parameters
+#' Model 2:
 #'
-#' @param t real value, time
-#' @param lambda_1 real value, first parameter of the exponential function
-#' @param lambda_2 real value, second parameter of the exponential function
-#' @param lambda_3 real value, third parameter of the exponential function
-#' @param beta_k real value, asymptotic stage
-#' @param alpha_1 real value, initial stage of the exponential function
+#' Recession with one exponential and four parameters: \deqn{\alpha \cdot \exp(-\lambda \cdot t^c) + \beta_k}
 #'
+#' @return Parameter's description:
+#' \enumerate{
+#'   \item{t real value, time}
+#'   \item{lambda_1 real value, first parameter of the exponential function}
+#'   \item{beta_k real value, asymptotic stage}
+#'   \item{alpha_1 real value, initial stage of the exponential function}
+#'   \item{c real value, recession exponent}
+#' }
 #' @export
-Recession_BR1_Equation <- function(t,alpha_1,lambda_1,lambda_2,lambda_3,beta_k){
-  alpha_1*exp(-lambda_1*t-lambda_2*t^2-lambda_3*t^0.5)+beta_k
-}
-
-#' Recession with one exponential and four parameters
-#'
-#' @param t real value, time
-#' @param lambda_1 real value, first parameter of the exponential function
-#' @param beta_k real value, asymptotic stage
-#' @param alpha_1 real value, initial stage of the exponential function
-#' @param c real value, recession exponent
-#'
-#' @export
-Recession_B2_Equation <- function(t,alpha_1,lambda_1,c,beta_k){
-  alpha_1*exp(-lambda_1*t^c)+beta_k
+Recession_B2_Equation <- function(){
+  return('alpha*exp(-lambda*t^c)+beta_k')
 }
